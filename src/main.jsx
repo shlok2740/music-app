1ac3b658cde7297371b94f5952db82a2a47fb350
Shlok Kumar <shlokkumar2303@gmail.com>
2024-08-30 11:59:03 +0530

    third commit p2

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,10 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
+import { injectSpeedInsights } from "@vercel/speed-insights";
+
+injectSpeedInsights();
+
 createRoot(document.getElementById("root")).render(
     <StrictMode>
         <App />
